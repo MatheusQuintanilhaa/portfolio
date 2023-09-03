7d7c8a09871cfe8efcc7b22cfeddf5223461316f
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2023-09-03 20:23:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PersonalInformations from "./components/PersonalInformations";
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      <PersonalInformations />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PersonalInformations.jsx
@@ -0,0 +1,11 @@
+const PersonalInformations = () => {
+  return (
+    <div className="app">
+      <section className="card">user</section>
+      <section className="card">about</section>
+      <section className="card projects">projects</section>
+    </div>
+  );
+};
+
+export default PersonalInformations;
