83ea5bf10f48f8b90dbc70fe0498ab3a9320f80d
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2023-09-04 16:31:17 -0300

    criando os projetos de forma dinamica

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,109 +1,52 @@
+import { useEffect, useState } from "react";
+
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await fetch("./projects.json");
+        if (!response.ok) {
+          throw new Error("Não foi possível buscar os projetos");
+        }
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProjects();
+  }, []);
+
   return (
     <section className="card projects">
       <h2 className="subtitle">Projetos</h2>
 
       <div className="projects__content">
-        <a
-          href="https://github.com/MatheusQuintanilhaa/Dash-Dine-App"
-          target="_blank"
-          rel="noreferrer"
-          className="project"
-        >
-          <picture>
-            <img
-              src="../../../src/assets/dashdine.png"
-              alt="DashDine project app"
-            />
-          </picture>
-          <strong>Dash Dine delivery App</strong>
+        {projects.map((project, index) => (
+          <a
+            key={index}
+            href={project.link}
+            target="_blank"
+            rel="noreferrer"
+            className="project"
+          >
+            <picture>
+              <img
+                src={project.image}
+                alt={`Imagem do projeto ${project.name}`}
+              />
+            </picture>
+            <strong>{project.name}</strong>
 
-          <div>
-            <span>React JS</span>
-            <span>Tailwind CSS</span>
-            <span>Splide</span>
-          </div>
-        </a>
-        <a
-          href="https://github.com/MatheusQuintanilhaa/Projeto-Portfolio-Joao-Lemon-UX-UI"
-          target="_blank"
-          rel="noreferrer"
-          className="project"
-        >
-          <picture>
-            <img
-              src="../../../src/assets/joaolemon.png"
-              alt="DashDine project app"
-            />
-          </picture>
-          <strong>João Lemon UX-UI Portfolio</strong>
-
-          <div>
-            <span>HTML</span>
-            <span>CSS</span>
-            <span>JavaScript</span>
-          </div>
-        </a>
-        <a
-          href="https://github.com/MatheusQuintanilhaa/Metaverse"
-          target="_blank"
-          rel="noreferrer"
-          className="project"
-        >
-          <picture>
-            <img
-              src="../../../src/assets/metaverse.png"
-              alt="DashDine project app"
-            />
-          </picture>
-          <strong>Metaversus</strong>
-
-          <div>
-            <span>React JS</span>
-            <span>Tailwind CSS</span>
-            <span>Framer Motion</span>
-          </div>
-        </a>
-        <a
-          href="https://github.com/MatheusQuintanilhaa/Flight-Booking-Website"
-          target="_blank"
-          rel="noreferrer"
-          className="project"
-        >
-          <picture>
-            <img
-              src="../../../src/assets/flyAgency.png"
-              alt="DashDine project app"
-            />
-          </picture>
-          <strong>Dash Dine delivery App</strong>
-
-          <div>
-            <span>React JS</span>
-            <span>SASS</span>
-            <span>Core JS</span>
-          </div>
-        </a>
-        <a
-          href="https://github.com/MatheusQuintanilhaa/Bistro-cafe"
-          target="_blank"
-          rel="noreferrer"
-          className="project"
-        >
-          <picture>
-            <img
-              src="../../../src/assets/bistroCafe.png"
-              alt="Bistro Cafe project app"
-            />
-          </picture>
-          <strong>Bistro Cafe</strong>
-
-          <div>
-            <span>React JS</span>
-            <span>Tailwind CSS</span>
-            <span>Framer Motion</span>
-          </div>
-        </a>
+            <div>
+              {project.tags.map((tag, tagIndex) => (
+                <span key={tagIndex}>{tag}</span>
+              ))}
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
