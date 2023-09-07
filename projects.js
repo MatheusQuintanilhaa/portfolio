94107105dd4db0ea5214b4d98aa4bf49565226f3
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2023-09-07 17:48:07 -0300

    alterando as rotas das imagens

--- projects.js
+++ projects.js
@@ -1,42 +1,49 @@
+import nike from "../../../src/assets/nikeWebsite.png";
+import dashdine from "../../../src/assets/dashdine.png";
+import joaolemon from "../../../src/assets/joaolemon.png";
+import metaverse from "../../../src/assets/metaverse.png";
+import flyAgency from "../../../src/assets/flyAgency.png";
+import bistroCafe from "../../../src/assets/bistroCafe.png";
+
 const projetos = [
   {
     id: 1,
     name: "Nike Website",
-    image: "../../../src/assets/nikeWebsite.png",
+    image: nike,
     tags: ["React JS", "Tailwind CSS", "Vite"],
     link: "https://github.com/MatheusQuintanilhaa/Nike-Website",
   },
   {
     id: 2,
     name: "Dash Dine Delivery",
-    image: "../../../src/assets/dashdine.png",
+    image: dashdine,
     tags: ["React JS", "Tailwind CSS", "Splide"],
     link: "https://github.com/MatheusQuintanilhaa/Dash-Dine-App",
   },
   {
     id: 3,
     name: "João Lemon UX-UI Portfolio",
-    image: "../../../src/assets/joaolemon.png",
+    image: joaolemon,
     tags: ["HTML", "CSS", "JavaScript"],
     link: "https://github.com/MatheusQuintanilhaa/Projeto-Portfolio-Joao-Lemon-UX-UI",
   },
   {
     id: 4,
     name: "Metaversus",
-    image: "../../../src/assets/metaverse.png",
+    image: metaverse,
     tags: ["React JS", "Tailwind CSS", "Framer Motion"],
     link: "https://github.com/MatheusQuintanilhaa/Metaverse",
   },
   {
     id: 5,
-    name: "Flight Booking Website",
+    name: flyAgency,
     image: "../../../src/assets/flyAgency.png",
     tags: ["React JS", "SASS", "Core JS"],
     link: "https://github.com/MatheusQuintanilhaa/Flight-Booking-Website",
   },
   {
     id: 6,
-    name: "Bistro Cafe",
+    name: bistroCafe,
     image: "../../../src/assets/bistroCafe.png",
     tags: ["React JS", "Tailwind CSS", "Framer Motion"],
     link: "https://github.com/MatheusQuintanilhaa/Bistro-cafe",
